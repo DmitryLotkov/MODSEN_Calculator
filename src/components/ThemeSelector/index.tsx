ccdbcba52fff6e933dcdf30e08b68262793ab332
DmitryLotkov <dmitry.lotkov9@gmail.com>
2022-10-25 23:58:28 +0300

    Added react import in themeSelector

--- src/components/ThemeSelector/index.tsx
+++ src/components/ThemeSelector/index.tsx
@@ -1,10 +1,11 @@
 import * as Styled from "./component"
 import { ControlPanelPropsType, ThemeType } from "../../types"
 import { ChangeEvent } from "react"
-
+import React from 'react'
 
 export const ThemeSelectorFC = ({ switchTheme, appTheme }: ControlPanelPropsType) => {
-  const onChangeHandler = (e:ChangeEvent<HTMLSelectElement>) =>{
+  const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    // eslint-disable-next-line prettier/prettier
     switchTheme(e.currentTarget.value as ThemeType)
   }
   return (
@@ -13,8 +14,9 @@ export const ThemeSelectorFC = ({ switchTheme, appTheme }: ControlPanelPropsType
       value={appTheme}>
       <Styled.OptionChange value="light">
         Light Theme
-      </Styled.OptionChange><Styled.OptionChange value="colored">
-        Colored Theme
+      </Styled.OptionChange>
+      <Styled.OptionChange value="colored">
+      Colored Theme
       </Styled.OptionChange>
       <Styled.OptionChange value="dark">
         Dark Theme
